bd19bad7a4f7b07202c8295c41175059d5b2fa1c
s1260054@u-aizu.ac.jp <s1260054@u-aizu.ac.jp>
2020-12-03 05:34:38 +0900

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,12 +7,13 @@
     <div class="w-auto">
         <p class="description">
             「Let-me-Know」はアンケートフォームを作成し、回答用フォームのURLをSNSや特定のメンバーに共有し回答をもらうことができます。<br><br>
-            <span class="subtitle">「Let-me-Know」が優れている点：最低限の機能があり、同様のアプリに比べてシンプルな画面で操作がしやすいのが特徴です。</span><br><br>
+            <span class="subtitle">「Let-me-Know」が優れている点：</span>
+            最低限の機能があり、同様のアプリに比べてシンプルな画面で操作がしやすいのが特徴です。<br><br>
             <span class="subtitle">「Let-me-Know」の作成した経緯：</span><br>
             ① フレームワークを用いたCRUDサイト制作に取り組みたいと思った<br>
             ② 自己分析を始めるにあたって長所・短所など気になることを気軽に聞きたいと思ったため<br>
             　(私の大学ではTwitter利用者が多く、Twitterにはそのような機能がなかったため)<br>
-            ③ 一つの画面で回答数の確認やリンクのコピーなどできれば利用者的に使いやすいと感じたため<br>
+            ③ 一つの画面で回答数の確認やリンクのコピーなどできれば利用者的に使いやすいと感じたため<br><br>
             <span class="subtitle">環境/使用言語：</span><br>
             Docker / PHP(Laravel)+MySQL
         </p>
